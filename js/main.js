e0f9967f9ddf9e111522e9f670840cb46b875a4e
coluccifrancesco <coluccif2002@gmail.com>
2025-05-28 13:27:23 +0200

    Refactor FizzBuzz program implementation in main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // Consegna: scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 for (let i = 1; i <= 100; i++) {
@@ -14,7 +13,7 @@ for (let i = 1; i <= 100; i++) {
     } else {
 
         console.log(i)
-        
+
     }
     
     if (i % 3 == 0 && i % 5 == 0){
