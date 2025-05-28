9786fa3691a2055deef95925ef59d093eab56fa0
coluccifrancesco <coluccif2002@gmail.com>
2025-05-28 13:29:57 +0200

    Update FizzBuzz program implementation in main.js for logic error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@
 
 for (let i = 1; i <= 100; i++) {
     
-    if (i % 3 == 0){
+    if (i % 3 == 0 && i % 5 == 0){
+        
+        console.log("Fizz Buzz")
+    
+    } else if(i % 3 == 0){
     
         console.log("Fizz")
     
@@ -15,10 +19,6 @@ for (let i = 1; i <= 100; i++) {
         console.log(i)
 
     }
-    
-    if (i % 3 == 0 && i % 5 == 0){
-        
-        console.log("Fizz Buzz")
-    
-    }
 }
+
+// Trova un bonus carino
